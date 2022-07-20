ffee940b076d4ac9b3d6ff1e2e51b68db5c1d6e7
GustBitencourt <gubiten@gmail.com>
2022-07-20 11:30:07 -0300

    types e metro config pra carregar SVS, componentes de input e button, logica inicial  pra pegar email e senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,50 @@
-import { Heading, VStack } from "native-base";
+import { useState } from 'react';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
+import { Envelope, Key  } from 'phosphor-react-native'
+
+import Logo from '../../assets/logo_primary.svg';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
 export function SignIn() {
-    return (
-        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
-            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-                SignIn
-            </Heading>
-        </VStack>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  //hook para obter as cores do theme a alterar icones
+  const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log('email: ', email, ' password: ', password);
+  }
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+
+      <Input 
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]} />} />}
+        onChangeText={setEmail}
+      />      
+      <Input 
+        placeholder="Senha"
+        mb={8}
+        InputLeftElement={<Icon ml={4} as={<Key color={colors.gray[300]} />} />}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+
+      <Button 
+        title='Entrar'
+        w='full'    
+        onPress={handleSignIn}  
+      />
+    </VStack>
+  )
 }
