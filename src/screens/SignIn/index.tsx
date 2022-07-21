bb3f0c1c381c65259fe9b5e04a120477ddb02b49
GustBitencourt <gubiten@gmail.com>
2022-07-20 21:03:58 -0300

    configuracao de autenticacao do usuario

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth'
+import { Alert } from 'react-native';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key  } from 'phosphor-react-native'
 
@@ -10,12 +12,38 @@ import { Button } from '../../components/Button';
 export function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   //hook para obter as cores do theme a alterar icones
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log('email: ', email, ' password: ', password);
+    if(!email || !password) {
+      return Alert.alert('Entrar', 'Informe email');
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if(error.code === 'auth/invalid-email') {
+          return Alert.alert('Entrar', 'Email inválido.');
+        }
+
+        if(error.code === 'auth/wrong-password') {
+          return Alert.alert('Entrar', 'Email ou senha inválida.');
+        }
+
+        if(error.code === 'auth/user-not-found') {
+          return Alert.alert('Entrar', 'Email ou senha inválida.');
+        }
+
+        return Alert.alert('Entrar', 'Não foi possível entrar.');
+      })
   }
 
   return (
@@ -43,7 +71,8 @@ export function SignIn() {
       <Button 
         title='Entrar'
         w='full'    
-        onPress={handleSignIn}  
+        onPress={handleSignIn}
+        isLoading={isLoading}
       />
     </VStack>
   )
